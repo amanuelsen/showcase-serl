3f814ecf24cfd9d482086785aed79a7b85f6d3f9
senai <saniy.cr7@gmail.com>
2024-09-11 15:24:47 +0100

    changed the import Qrcode

--- src/app/Components/ProjectClientComponent.tsx
+++ src/app/Components/ProjectClientComponent.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { useState, useEffect } from 'react'
-import QRCode from 'qrcode'
+import QRCode from 'qrcode.react'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 
