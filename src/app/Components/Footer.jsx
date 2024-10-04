06c467efd2cf2f64ae840213ab48609ccd723f93
Senai Amanuel <128589263+amanuelsen@users.noreply.github.com>
2024-10-04 11:30:29 +0100

    added some social links

--- src/app/Components/Footer.jsx
+++ src/app/Components/Footer.jsx
@@ -1,6 +1,8 @@
 'use client'
 import React from 'react'
 import { Box, Typography, Link, Container } from '@mui/material'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
+import GitHubIcon from '@mui/icons-material/GitHub'
 
 export default function Footer() {
   return (
@@ -10,17 +12,24 @@ export default function Footer() {
         py: 3,
         px: 2,
         mt: 'auto',
+
         backgroundColor: '#1976d2',
       }}
     >
-      <Container maxWidth='lg'>
-        <Typography variant='h6' align='center' gutterBottom>
-          ShowcaseSERL
-        </Typography>
-        <Typography variant='body2' color='text.secondary' align='center'>
+      <Container
+        maxWidth='lg'
+        style={{
+          display: 'flex',
+          justifyContent: 'space-evenly',
+          alignItems: 'center',
+          alignContent: 'center',
+        }}
+      >
+        <Typography variant='body4' color='text.secondary' align='center'>
           {'© '}
           {new Date().getFullYear()} {'All rights reserved.'}
         </Typography>
+
         <Box
           sx={{
             display: 'flex',
@@ -37,6 +46,33 @@ export default function Footer() {
           </Link>
           <Link href='/contact' color='inherit' sx={{ mx: 2 }}>
             Contact
+          </Link>
+        </Box>
+
+        {/* Social Links */}
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            mt: 2,
+          }}
+        >
+          <Link
+            href='https://www.linkedin.com/in/senai-amanuel-769016273/'
+            target='_blank'
+            color='inherit'
+            sx={{ mx: 2 }}
+          >
+            <LinkedInIcon fontSize='large' />
+          </Link>
+          <Link
+            href='https://github.com/amanuelsen'
+            target='_blank'
+            color='inherit'
+            sx={{ mx: 2 }}
+          >
+            <GitHubIcon fontSize='large' />
           </Link>
         </Box>
       </Container>
